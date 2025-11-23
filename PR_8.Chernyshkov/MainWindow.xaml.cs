18290fac40d901a455a534f2a1bf3aababd04bad
df275 <df275@EVIL>
2025-11-23 18:21:28 +0500

    MainWindow.xaml.cs Чернышков

--- PR_8.Chernyshkov/MainWindow.xaml.cs
+++ PR_8.Chernyshkov/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PR_8.Chernyshkov.Classes;
 
 namespace PR_8.Chernyshkov
 {
@@ -20,9 +21,39 @@ namespace PR_8.Chernyshkov
     /// </summary>
     public partial class MainWindow : Window
     {
+        private LiteVoin lightWarrior;
+        private ProVoin heavyWarrior;
         public MainWindow()
         {
             InitializeComponent();
+
+            lightWarrior = new LiteVoin("Алексей", 100);
+            heavyWarrior = new ProVoin("Иван", 100);
+
+            
+            UpdateWarriorInfo();
+
         }
+        private void UpdateWarriorInfo()
+        {
+            LightInfo.Text = $"Легкий воин\nЗдоровье: {lightWarrior.Health}/{lightWarrior.MaxHealth}";
+            HeavyInfo.Text = $"Тяжелый воин\nЗдоровье: {heavyWarrior.Health}/{heavyWarrior.MaxHealth}";
+        }
+
+        private void BtnLight_Click(object sender, RoutedEventArgs e)
+        {
+            lightWarrior.TakeDamage(20);
+            UpdateWarriorInfo();
+            lightWarrior.ShowInfo();
+        }
+
+        private void BtnHeavy_Click(object sender, RoutedEventArgs e)
+        {
+            heavyWarrior.TakeDamage(20);
+            UpdateWarriorInfo();
+            heavyWarrior.ShowInfo();
+        }
+
+
     }
 }
