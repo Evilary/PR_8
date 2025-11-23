adbd257232e4e62be2357bfa6c04c6768fe643f7
df275 <df275@EVIL>
2025-11-23 17:04:29 +0500

    Класс Voin. Чернышков

---
+++ PR_8.Chernyshkov/Classes/Voin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_8.Chernyshkov.Classes
+{
+    public class Voin
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+
+        public Voin(string name, int maxHealth) 
+        {
+        
+            Name = name;
+            MaxHealth = maxHealth;
+            Health = maxHealth;
+        
+        
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 0) Health = 0;
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Воин: {Name}");
+            Console.WriteLine($"Здоровье: {Health}/{MaxHealth}");
+        }
+
+    }
+}
